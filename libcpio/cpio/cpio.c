06ff1cb1accd5b003c26122c6fcef0d4c7db1446
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2020-11-19 10:36:48 +0100

    libcpio: fix alignment; add checking end of file

--- libcpio/cpio/cpio.c
+++ libcpio/cpio/cpio.c
@@ -30,181 +30,187 @@
 
 
 typedef struct {
-    char c_magic[6];
-    char c_ino[8];
-    char c_mode[8];
-    char c_uid[8];
-    char c_gid[8];
-    char c_nlink[8];
-    char c_mtime[8];
-    char c_filesize[8];
-    char c_devmajor[8];
-    char c_devminor[8];
-    char c_rdevmajor[8];
-    char c_rdevminor[8];
-    char c_namesize[8];
-    char c_check[8];
-    char name[];
+	char c_magic[6];
+	char c_ino[8];
+	char c_mode[8];
+	char c_uid[8];
+	char c_gid[8];
+	char c_nlink[8];
+	char c_mtime[8];
+	char c_filesize[8];
+	char c_devmajor[8];
+	char c_devminor[8];
+	char c_rdevmajor[8];
+	char c_rdevminor[8];
+	char c_namesize[8];
+	char c_check[8];
+	char name[];
 } cpio_newc_t;
 
 
 static uint32_t cpio_a2i(const char *s)
 {
-    uint32_t i, k = 28, v = 0;
-    char d;
+	uint32_t i, k = 28, v = 0;
+	char d;
 
-    for (i = 0; (i < 8) && s[i]; i++) {
-        d = s[i] - '0';
+	for (i = 0; (i < 8) && s[i]; i++) {
+		d = s[i] - '0';
 
-        if ((d > 16) && (d < 23))
-            d -= 7;
-        else if ((d > 48) && (d < 55))
-            d -= 39;
-        else if (d > 9)
-            return -1;
+		if ((d > 16) && (d < 23))
+			d -= 7;
+		else if ((d > 48) && (d < 55))
+			d -= 39;
+		else if (d > 9)
+			return -1;
 
-        v += (d << k);
-        k -= 4;
-    }
+		v += (d << k);
+		k -= 4;
+	}
 
-    return v;
+	return v;
 }
 
 
 static int cpio_readData(oid_t oid, size_t offs, size_t size, void *data)
 {
-    msg_t msg;
+	msg_t msg;
 
-    msg.type = mtRead;
-    msg.i.io.oid = oid;
-    msg.i.io.offs = offs;
-    msg.i.data = NULL;
-    msg.i.size = 0;
-    msg.o.data = data;
-    msg.o.size = size;
+	msg.type = mtRead;
+	msg.i.io.oid = oid;
+	msg.i.io.offs = offs;
+	msg.i.data = NULL;
+	msg.i.size = 0;
+	msg.o.data = data;
+	msg.o.size = size;
 
-    if (msgSend(oid.port, &msg) != 0 )
-        return -1;
+	if (msgSend(oid.port, &msg) != 0 )
+		return -1;
 
-    if (msg.o.io.err < size)
-        return -1;
+	if (msg.o.io.err < size)
+		return -1;
 
-    return 0;
+	return 0;
 }
 
 
 int cpio_findFile(const char *filepath, cpio_file_t *fd)
 {
-    cpio_newc_t cpio;
-    int firstPass = 1;
-    uint32_t offs = 0, fs = 0, ns = 0, rpt = 0;
-    char *dir, *filename, *pathBuff;
-    char nameBuff[MAX_FILE_NAME_SIZE];
+	cpio_newc_t cpio;
+	int firstPass = 1;
+	uint32_t offs = 0, fs = 0, ns = 0, rpt = 0;
+	char *dir, *filename, *pathBuff;
+	char nameBuff[MAX_FILE_NAME_SIZE];
 
-    if ((pathBuff = strdup(filepath)) == NULL)
-        return -1;
+	if ((pathBuff = strdup(filepath)) == NULL)
+		return -1;
 
-    memset(nameBuff, 0, MAX_FILE_NAME_SIZE);
+	memset(nameBuff, 0, MAX_FILE_NAME_SIZE);
 
-    filename = basename(pathBuff);
-    dir = dirname(pathBuff);
+	filename = basename(pathBuff);
+	dir = dirname(pathBuff);
 
-    /* Connect to flashsrv */
-    while (lookup(dir, NULL, &fd->oid) < 0) {
-        usleep(10000);
-        if (rpt++ > MAX_REPETITION_NB) {
-            free(pathBuff);
-            return -1;
-        }
-    }
+	/* Connect to flashsrv */
+	while (lookup(dir, NULL, &fd->oid) < 0) {
+		usleep(10000);
+		if (rpt++ > MAX_REPETITION_NB) {
+			free(pathBuff);
+			return -1;
+		}
+	}
 
-    /* Read headers */
-    for (;;) {
-        cpio_readData(fd->oid, offs, sizeof(cpio_newc_t), &cpio);
-        if (strncmp(cpio.c_magic, MAGIC_BYTES_NEWC, 6) != 0) {
-            free(pathBuff);
-            return -1;
-        }
+	/* Read headers */
+	for (;;) {
+		cpio_readData(fd->oid, offs, sizeof(cpio_newc_t), &cpio);
+		if (strncmp(cpio.c_magic, MAGIC_BYTES_NEWC, 6) != 0) {
+			free(pathBuff);
+			return -1;
+		}
 
-        ns = cpio_a2i(cpio.c_namesize);
-        fs = cpio_a2i(cpio.c_filesize);
+		ns = cpio_a2i(cpio.c_namesize);
+		fs = cpio_a2i(cpio.c_filesize);
 
-        offs += sizeof(cpio_newc_t);
-        if (ns > MAX_FILE_NAME_SIZE || cpio_readData(fd->oid, offs, ns, nameBuff) < 0) {
-            free(pathBuff);
-            return -1;
-        }
+		offs += sizeof(cpio_newc_t);
+		if (ns > MAX_FILE_NAME_SIZE || cpio_readData(fd->oid, offs, ns, nameBuff) < 0) {
+			free(pathBuff);
+			return -1;
+		}
 
-        if (strcmp(nameBuff, END_OF_ARCHIVE) == 0) {
-            free(pathBuff);
-            return -1;
-        }
+		if (strcmp(nameBuff, END_OF_ARCHIVE) == 0) {
+			free(pathBuff);
+			return -1;
+		}
 
-        offs = (offs + ns + CPIO_PAD) & ~CPIO_PAD;
+		offs = (offs + ns + CPIO_PAD) & ~CPIO_PAD;
 
-        if (firstPass) {
-            firstPass = 0;
-            continue;
-        }
+		if (firstPass) {
+			firstPass = 0;
+			continue;
+		}
 
-        if (strncmp(nameBuff, filename, ns) == 0) {
-            fd->size = fs;
-            fd->offs = offs;
-            fd->currPos = 0;
-            break;
-        }
+		if (strncmp(nameBuff, filename, ns) == 0) {
+			fd->size = fs;
+			fd->offs = offs;
+			fd->currPos = 0;
+			break;
+		}
 
-        offs = (offs + fs + CPIO_PAD) & ~CPIO_PAD;
-    }
+		offs = (offs + fs + CPIO_PAD) & ~CPIO_PAD;
+	}
 
-    free(pathBuff);
+	free(pathBuff);
 
-    return 0;
+	return 0;
 }
 
 
 ssize_t cpio_readFile(char *buff, size_t size, cpio_file_t *fd)
 {
-    uint32_t sz;
+	uint32_t sz;
 
-    if (fd->currPos + size > fd->size)
-        sz = fd->size - fd->currPos;
-    else
-        sz = size;
+	if (fd->currPos + size > fd->size)
+		sz = fd->size - fd->currPos;
+	else
+		sz = size;
 
-    if (cpio_readData(fd->oid, fd->offs + fd->currPos, sz, buff) < 0)
-        return -1;
+	if (cpio_readData(fd->oid, fd->offs + fd->currPos, sz, buff) < 0)
+		return -1;
 
-    fd->currPos += sz;
+	fd->currPos += sz;
 
-    return sz;
+	return sz;
 }
 
 
 int cpio_seekFile(cpio_file_t *fd, uint32_t offset, int origin)
 {
-    switch (origin) {
-        case cpio_seek_cur:
-            if (fd->currPos + offset > fd->size)
-                return -1;
-            fd->currPos += offset;
-            break;
+	switch (origin) {
+		case cpio_seek_cur:
+			if (fd->currPos + offset > fd->size)
+				return -1;
+			fd->currPos += offset;
+			break;
 
-        case cpio_seek_end:
-            if (offset > fd->size)
-                return -1;
-            fd->currPos = fd->size - offset;
-            break;
+		case cpio_seek_end:
+			if (offset > fd->size)
+				return -1;
+			fd->currPos = fd->size - offset;
+			break;
 
-        case cpio_seek_set:
-            if (offset > fd->size)
-                return -1;
-            fd->currPos = offset;
-            break;
+		case cpio_seek_set:
+			if (offset > fd->size)
+				return -1;
+			fd->currPos = offset;
+			break;
 
-        default:
-            return -1;
-    }
+		default:
+			return -1;
+	}
 
-    return 0;
+	return 0;
+}
+
+
+int cpio_endOfFile(const cpio_file_t *fd)
+{
+	return fd->size == fd->currPos;
 }
--- libcpio/cpio/cpio.h
+++ libcpio/cpio/cpio.h
@@ -18,19 +18,25 @@
 
 
 typedef struct {
-    uint32_t offs;
-    uint32_t currPos;
-    uint32_t size;
+	uint32_t offs;
+	uint32_t currPos;
+	uint32_t size;
 
-    oid_t oid;
+	oid_t oid;
 } cpio_file_t;
 
 
 enum { cpio_seek_set = 0 /* beginnig of file */, cpio_seek_cur /* current position of file */, cpio_seek_end /* end of file */};
 
 
+
 int cpio_findFile(const char *filepath, cpio_file_t *fd);
+
 
 ssize_t cpio_readFile(char *buff, size_t size, cpio_file_t *fd);
 
+
 int cpio_seekFile(cpio_file_t *fd, uint32_t offset, int origin);
+
+
+int cpio_endOfFile(const cpio_file_t *fd);
